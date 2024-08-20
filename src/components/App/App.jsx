109245a5b82c109088b30335d0ed9669cbe0ac52
Vladislav Pavlov <vldslw@gmail.com>
2024-08-20 20:07:29 -0300

    feat: add initial content

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,20 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import Header from "../Header/Header";
+import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />} />
+      </Routes>
     </div>
   );
 }
