e853595b9994ec57bf20bc7afc1eb8507d67cf83
Vladislav Pavlov <vldslw@gmail.com>
2024-08-21 11:59:10 -0300

    feat: add initial content

---
+++ src/components/AboutUs/AboutUs.jsx
@@ -0,0 +1,11 @@
+import "./AboutUs.css";
+
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <p>You can find out more information about our site here.</p>
+    </div>
+  );
+}
+
+export default AboutUs;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,22 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import Header from "../Header/Header";
+import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
+import AboutUs from "../AboutUs/AboutUs";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/about-us" element={<AboutUs />} />
+      </Routes>
     </div>
   );
 }
