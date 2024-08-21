ba8862139769f8ea37d6004c75382103d166a03c
Vladislav Pavlov <vldslw@gmail.com>
2024-08-21 12:03:11 -0300

    feat: add initial content

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,34 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
 import Header from "../Header/Header";
+import Dashboard from "../Dashboard/Dashboard";
+import Reviews from "../Reviews/Reviews";
+import AboutMe from "../AboutMe/AboutMe";
+import Contact from "../AboutMe/Contact";
+import Hobbies from "../AboutMe/Hobbies";
+import MyStory from "../AboutMe/MyStory";
+import AboutUs from "../AboutUs/AboutUs";
+import SiteHistory from "../AboutUs/SiteHistory";
+import SiteMission from "../AboutUs/SiteMission";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/about-me" element={<AboutMe />}>
+          <Route path="contact" element={<Contact />} />
+          <Route path="hobbies" element={<Hobbies />} />
+          <Route path="my-story" element={<MyStory />} />
+        </Route>
+        <Route path="/about-us" element={<AboutUs />}>
+          <Route path="mission" element={<SiteMission />} />
+          <Route path="history" element={<SiteHistory />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import "./Header.css";
 import Logo from "../Logo/Logo";
+import NavBar from "../NavBar/NavBar";
 
 function Header() {
   return (
     <div className="header">
       <Logo />
+      <NavBar />
     </div>
   );
 }
