6782666f2a64538eb8c35d15ee42151d21c75cc5
Hernán Baravalle <hernan.baravalle@gmail.com>
2024-09-23 02:29:23 -0300

    refactor: move Main component to root components directory

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 
-import Header from "../Header/Header";
-import Dashboard from "../Dashboard/Dashboard";
-import Reviews from "../Reviews/Reviews";
-import AboutMe from "../AboutMe/AboutMe";
-import AboutUs from "../AboutUs/AboutUs";
+import Header from "./Header/Header";
+import Dashboard from "./Dashboard/Dashboard";
+import Reviews from "./Reviews/Reviews";
+import AboutMe from "./AboutMe/AboutMe";
+import AboutUs from "./AboutUs/AboutUs";
 import "./App.css";
 
 function App() {
