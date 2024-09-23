e7f95e2246c36814bb5b95930ab3b37bcaa08be1
Vladislav Pavlov <vldslw@gmail.com>
2024-09-23 18:33:23 -0300

    fix base route

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-router-demo-es">
+    <BrowserRouter basename="/react-router-demo-es/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
